7339e39f1cd578ba05b9142694483b16edcfb18b
Tavirutyutyu <erdosmarci@gmail.com>
2025-07-05 20:31:49 +0200

    add instrumentType and hasResonator in InstrumentDTO

--- backend/src/main/java/org/stonebass/backend/DTO/InstrumentDTO.java
+++ backend/src/main/java/org/stonebass/backend/DTO/InstrumentDTO.java
@@ -2,5 +2,13 @@ package org.stonebass.backend.DTO;
 
 import java.util.List;
 
-public record InstrumentDTO(Long id, String title, String description, List<String> images, String youtubeLink) {
+public record InstrumentDTO(
+        Long id,
+        String title,
+        String description,
+        String instrumentType,
+        boolean hasResonator,
+        List<String> images,
+        String youtubeLink
+) {
 }
