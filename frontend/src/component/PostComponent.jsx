9e53761f73e985028220dcee09e650bf7637f08a
Tavirutyutyu <erdosmarci@gmail.com>
2025-07-05 18:33:39 +0200

    add youtube video component on frontend

--- frontend/src/component/PostComponent.jsx
+++ frontend/src/component/PostComponent.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import YouTubeVideo from "./YoutubeVideo.jsx";
 
 export default function PostComponent({post, showControls = false}) {
     const imageSrcString = "data:image/png;base64,";
@@ -42,11 +43,12 @@ export default function PostComponent({post, showControls = false}) {
         <div id='post' key={title} onClick={handleClick}>
             <h1 id={"title"}>{title}</h1>
             <img id={"postImage"} src={currentImage} alt={"No Image Found"}></img>
-            <p id={"postDescription"}>{description}</p>
             {showControls && (
                 <div id='controls'>
                     <button onClick={previousImage}>Previous</button>
                     <button onClick={nextImage}>Next</button>
+                    <p id={"postDescription"}>{description}</p>
+                    <YouTubeVideo videoId={"w0drLyhnByk"}/>
                 </div>
             )}
         </div>
