0b101a39e806450ed550063904d91c6b65b789cd
Tavirutyutyu <erdosmarci@gmail.com>
2025-07-05 20:31:11 +0200

    add editor site

--- frontend/src/page/MainPage.jsx
+++ frontend/src/page/MainPage.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import '../mainPage.css'
 import PostComponent from "../component/PostComponent.jsx";
 import SearchForm from "../component/SearchForm.jsx";
+import {useNavigate} from "react-router-dom";
 
 async function getAllPosts() {
     try {
@@ -15,7 +16,8 @@ async function getAllPosts() {
     }
 }
 
-export default function MainPage() {
+export default function MainPage({isAdminPage = false}) {
+    const navigate = useNavigate();
     const [posts, setPosts] = useState(null)
     const [loading, setLoading] = useState(true)
 
@@ -28,6 +30,21 @@ export default function MainPage() {
         })
     }, [])
 
+    function handleEdit(e, post){
+        e.preventDefault()
+        navigate("/edit", {
+            state: {
+                title: post.title,
+                description: post.description,
+                files: post.images,
+                hasResonator: post.hasResonator,
+                instrumentType: post.instrumentType,
+                youtubeLink: post.youtubeLink
+            }
+        })
+
+    }
+
     if (loading) {
         return <p>Loading...</p>
     }
@@ -35,7 +52,14 @@ export default function MainPage() {
     return (
         <>
             <SearchForm/>
-            {posts?.map((post) => <PostComponent post={post}/>)}
+            {posts?.map((post) => {
+                return (
+                    <>
+                        <PostComponent post={post}/>
+                        {isAdminPage && <button type={"button"} onClick={(e) => handleEdit(e, post)}>Edit</button>}
+                    </>
+                )
+            })}
         </>
     )
 }
