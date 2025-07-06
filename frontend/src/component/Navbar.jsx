b4363e3aa0fa873df884b10842ff48a35e3ee7f8
tavirutyutyu <erdosmarci@gmail.com>
2025-07-06 16:55:01 +0200

    refactor nav bar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,26 +1,24 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 import logo from "/src/assets/logo.svg";
 
 export default function Navbar() {
     return (
         <>
             <nav>
-                <div>
+                <Link to="/">
                     <h1>StoneBass</h1>
-                    <img src={logo} alt="StoneBass" />
-                </div>
-                <div>
-                    <ul>
-                        <li>
-                            <a href="/instruments?hasResonator=true">Resonator</a>
-                        </li>
-                        <li>
-                            <a href="/instruments?hasResonator=false">Traditional</a>
-                        </li>
-                        <li>History</li>
-                        <li>About Me</li>
-                    </ul>
-                </div>
+                    <img src={logo} alt="StoneBass"/>
+                </Link>
+                <ul>
+                    <li>
+                        <a href="/instruments?hasResonator=true">Resonator</a>
+                    </li>
+                    <li>
+                        <a href="/instruments?hasResonator=false">Traditional</a>
+                    </li>
+                    <li>History</li>
+                    <li>About Me</li>
+                </ul>
             </nav>
             <Outlet/>
         </>
