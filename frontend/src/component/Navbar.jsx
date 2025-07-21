b819a269879637b65e8ba711d28b15efd32024d3
tavirutyutyu <erdosmarci@gmail.com>
2025-07-21 10:31:06 +0200

    add logo and style for nav bar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,33 +1,30 @@
-import {Link, Outlet} from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import logo from "/src/assets/logo.svg";
+import fejlec_szoveg from "/src/assets/fejlec_szoveg.svg";
 import "/src/style/navBar.css";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <>
             <nav>
-                <div className="navbar-logo">
-                    <Link id={"logo"} to="/">
-                        <img src={logo} alt="StoneBass"/>
-                        <h1>StoneBass</h1>
-                    </Link>
-                    <button className={"menu-toggle"} onClick={() => setIsOpen(!isOpen)}>
-                        ☰
-                    </button>
-                </div>
+                <Link id="logo" to="/">
+                    <img id="logo-icon" src={logo} alt="StoneBass icon" />
+                    <img id="logo-text" src={fejlec_szoveg} alt="StoneBass text" />
+                </Link>
+
+                <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+                    ☰
+                </button>
+
                 <ul className={isOpen ? "nav-links open" : "nav-links"}>
-                    <li>
-                        <a href="/gallery">Gallery</a>
-                    </li>
+                    <li><a href="/gallery">Gallery</a></li>
                     <li>History</li>
-                    <li>
-                        <a href="/aboutMe">About Me</a>
-                    </li>
+                    <li><a href="/aboutMe">About Me</a></li>
                 </ul>
             </nav>
-            <Outlet/>
+            <Outlet />
         </>
-    )
+    );
 }
