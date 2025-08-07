860e2abde628999b6bd5a6c4f0da359af4ef746e
tavirutyutyu <erdosmarci@gmail.com>
2025-08-07 21:36:13 +0200

    put aboutMe data to backend

--- backend/src/main/java/org/stonebass/backend/model/AboutMeEntity.java
+++ backend/src/main/java/org/stonebass/backend/model/AboutMeEntity.java
@@ -15,6 +15,7 @@ public class AboutMeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+    public String title;
     @Column(columnDefinition = "TEXT")
     public String description;
     @Column(columnDefinition = "BYTEA")
@@ -35,4 +36,31 @@ public class AboutMeEntity {
     public String playingOn4Description;
     @Column(columnDefinition = "BYTEA")
     public byte[] playingOn4;
+
+    public AboutMeEntity(
+            String title,
+            String description,
+            byte[] profileImage,
+            byte[] playingOn1,
+            String playingOn1Description,
+            byte[] playingOn2,
+            String playingOn2Description,
+            byte[] playingOn3,
+            String playingOn3Description,
+            byte[] playingOn4,
+            String playingOn4Description
+    ) {
+        this.title = title;
+        this.description = description;
+        this.profileImage = profileImage;
+        this.playingOn1 = playingOn1;
+        this.playingOn1Description = playingOn1Description;
+        this.playingOn2 = playingOn2;
+        this.playingOn2Description = playingOn2Description;
+        this.playingOn3 = playingOn3;
+        this.playingOn3Description = playingOn3Description;
+        this.playingOn4 = playingOn4;
+        this.playingOn4Description = playingOn4Description;
+    }
+
 }
